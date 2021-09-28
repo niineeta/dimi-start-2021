6d85228712b74e1ba14b05b5032745f4bb8a7ba0
niineeta <ninetapubli@gmail.com>
2021-09-28 16:41:46 +0200

    update

--- guess-the-number-console/main.js
+++ guess-the-number-console/main.js
@@ -2,16 +2,20 @@ console.log('Hello user!')
 console.log('Try to guess the mistery number.')
 console.log('Use "submit(x)" to submit a number.')
 
-const misteryNumber = 45
+const misteryNumber = Math.round(Math.random() * 100)
 
 const submit = (x) => {
-    if (x < mysteryNumber) {
+    if (x < misteryNumber) {
         console.log('Too small.')
     }
-    if (x > mysteryNumber) {
+    if (x > misteryNumber) {
         console.log('Too big.')
     }
-    if (x == mysteryNumber) {
+    if (x == misteryNumber) {
         console.log('Hurray!')
     }
+}
+
+const cheat = () => {
+    console.log(`The mistery number is ${misteryNumber}`)
 }
