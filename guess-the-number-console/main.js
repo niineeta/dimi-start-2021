d930f7c15f3b6fa79254baccff6be9d8413a3a35
niineeta <ninetapubli@gmail.com>
2021-09-28 16:57:46 +0200

    update

--- guess-the-number-console/main.js
+++ guess-the-number-console/main.js
@@ -7,14 +7,17 @@ const misteryNumber = Math.round(Math.random() * 100)
 const submit = (x) => {
     if (x < misteryNumber) {
         console.log('Too small.')
+        document.body.innerHTML = '<h1>Too small.</h1>'
         document.body.style.backgroundColor = '#ff5722'
     }
     if (x > misteryNumber) {
         console.log('Too big.')
+        document.body.innerHTML = '<h1>Too big.</h1>'
         document.body.style.backgroundColor = '#03a9f4'
     }
     if (x == misteryNumber) {
         console.log('Hurray!')
+        document.body.innerHTML = '<h1>Hurray!.</h1>'
         document.body.style.backgroundColor = '#ffeb3b'
     }
 }
